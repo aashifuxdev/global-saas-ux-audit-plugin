ebc314bef3fc5aeafdea1e9a986b819f0f5212eb
aashifuxdev <aash.if.ux@gmail.com>
2025-11-17 12:18:10 +0000

    Updated manifest and vite config for correct build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
       input: {
-        ui: 'index.html'
+        ui: resolve(__dirname, "index.html"), // ✅ use index.html instead
+        code: resolve(__dirname, "src/code.ts"),
       },
       output: {
-        entryFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
-  }
-})
+        entryFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
